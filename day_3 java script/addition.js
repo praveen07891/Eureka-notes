1a2aade52221d1d29ec96c220cb473fc32e787ea
praveen07891 <jhonlee1431@gmail.com>
2025-07-05 13:16:50 -0400

    commit

---
+++ day_3 java script/addition.js
@@ -0,0 +1,5 @@
+let a = 10;
+let b = 20;
+console.log(`Addition of ${a} & ${b} is ${a + b}`);
+window.alert(`Addition of ${a} & ${b} is ${a + b}`);
+document.querySelector('h1').style.color = 'red';
